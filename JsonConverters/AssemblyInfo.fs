68ef09ba1226fc72cfc97cdd88458c40a32255eb
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-07-18 15:09:02 +0100

    Move Option Converter to new package/ project

---
+++ JsonConverters/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace JsonConverters.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("JsonConverters")>]
+[<assembly: AssemblyCompany("Scott Logic RnD DevOps Team")>]
+[<assembly: AssemblyDescription("Custom Json Converters to use with Newtonsoft.Json")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyProduct("Bristech.Srm.JsonConverters")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("352555d6-6ffe-49a4-b3b9-5fac7b153568")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("0.1.1.0")>]
+[<assembly: AssemblyFileVersion("0.1.1.0")>]
+
+do
+    ()
